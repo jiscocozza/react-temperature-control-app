94882f2eae177399bc77fe0d90226e9ec76c7701
Juan Ignacio Scocozza <juanimotion@Juans-MacBook-Pro.local>
2022-02-11 13:03:21 -0300

    mensaje

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(24);
+  const [redValue, setRedValue] = useState(153);
+  const [greenValue, setGreenValue] = useState(190);
+  const [blueValue, setBlueValue] = useState(102);
+  const [outerRingColor, setOuterRingColor] = useState(`rgba(${redValue}, ${greenValue}, ${blueValue}, 1)`);
+  
+  const addTemperature = () => {
+    if (temperatureValue > 39) return;
+    setTemperatureValue(temperatureValue + 1);
+    setRedValue(redValue + 6.375);
+    setGreenValue(greenValue - 6.375);
+    setBlueValue(blueValue - 6.375);
+    setOuterRingColor(`rgba(${redValue}, ${greenValue}, ${blueValue}, 1)`);    
+  }
+
+  const restTemperature = () => {
+    if (temperatureValue < 1) return
+    setTemperatureValue(temperatureValue - 1);
+    setRedValue(redValue - 6.375);
+    setGreenValue(greenValue + 6.375);
+    setBlueValue(blueValue + 6.375);
+    setOuterRingColor(`rgba(${redValue}, ${greenValue}, ${blueValue}, 1)`);
+    console.log(redValue, greenValue, blueValue);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+          <div className="outer-ring" style={{borderColor: outerRingColor}}>
+            <div className="temperature-container">
+              <h1 className="temperature">{temperatureValue}°</h1>
+            </div>
+          </div>
+          <div className="buttons-container">
+            <span className="add-button-extrude">
+              <button onClick={ ()=> addTemperature() }>+</button>
+            </span>
+            <span className="rest-button-extrude">
+              <button onClick={ ()=> restTemperature() }>-</button>
+            </span>
+            <span className="light"></span>
+          </div>
+        </div>
+        <span className="control-extrude">
+      </span>
     </div>
   );
 }
